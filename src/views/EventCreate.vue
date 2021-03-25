35f443f1754b80b9bd01040e207a03537d3c5c0f
Phan Quoc Thanh <thanhpq@Happy.local>
2021-03-25 11:50:58 +0700

    start using action and mutation

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
     <h1>Create event</h1>
+    <!-- <p>This is created by {{ user.id }}</p> -->
+    <!-- <p>There are {{ catLength }} categories</p> -->
+    <!-- <p>{{ getEventById(3) }}</p> -->
+    <!-- <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul> -->
   </div>
 </template>
 
 <script>
-export default {};
+import { mapState, mapGetters } from "vuex";
+export default {
+  computed: {
+    getEvent() {
+      return this.$store.getters.getEventById;
+    },
+    catLength() {
+      return this.$store.getters.catLength;
+    },
+    ...mapGetters(["getEventById"]),
+    ...mapState(["user", "categories"]),
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
