6fb2449815ccea7f989b9635939aab6bc0a258b1
Phan Quoc Thanh <thanhpq@Happy.local>
2021-03-26 10:56:07 +0700

    start using notification

--- src/store/index.js
+++ src/store/index.js
@@ -1,81 +1,22 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import EventService from "@/services/EventService.js";
+import * as user from "@/store/modules/user.js"
+import * as event from "@/store/modules/event.js"
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  modules: {
+    user,
+    event
+  },
   state: {
-    user: {id: "abc123", name: "Adam jarh"},
     categories: ["sustainability", "nature", "animal welfare", "housing", "education", "food", "community"],
     todos: [
       { id: 1, text: "...", done: true },
       { id: 2, text: "...", done: false },
       { id: 3, text: "...", done: true },
       { id: 4, text: "...", done: false },
-    ],
-    events: [],
-    eventsTotal: 0,
-    event: {}
-
-  },
-  mutations: {
-    ADD_EVENT(state, event){
-      state.events.push(event)
-    },
-    SET_EVENTS(state, events) {
-      state.events = events
-    },
-    SET_EVENTS_TOTAL(state, eventsTotal) {
-      state.eventsTotal = eventsTotal
-    },
-    SET_EVENT(state, event) {
-      state.event = event
-    }
-  },
-  actions: {
-    createEvent({commit}, event){
-      return EventService.postEvent(event).then(() => {
-        commit('ADD_EVENT', event)
-      })
-    },
-    fetchEvents({commit}, {perPage, page}) {
-      EventService.getEvents(perPage, page)
-      .then((response) => {
-        commit(
-          'SET_EVENTS_TOTAL',
-          parseInt(response.headers['x-total-count'])
-        )
-        console.log(1)
-        commit('SET_EVENTS',response.data);
-      })
-      .catch((error) => {
-        console.log("there was an error " + error.respone);
-      });
-    },
-    fetchEvent({ commit, getters }, id) {
-      var event = getters.getEventById(id) // See if we already have this event
-      if (event) { // If we do, set the event
-        commit('SET_EVENT', event)
-      } else {  // If not, get it with the API.
-        EventService.getEvent(id)
-          .then(response => {
-            commit('SET_EVENT', response.data)
-          })
-          .catch(error => {
-            console.log('There was an error:', error.response)
-          })
-      }
-    }
-  },
-  modules: {},
-  getters: {
-    catLength: state => state.categories.length,
-    doneTodos: state => state.todos.filter(todos => todos.done),
-    activeTods: state => state.todos.filter(todos => !todos.done),
-    getEventById: state => id => {
-      console.log(id)
-      return state.events.find(event => event.id === id)
-    },
-  },
+    ]
+  }
 });
---
+++ src/store/modules/event.js
@@ -0,0 +1,85 @@
+import EventService from "@/services/EventService.js";
+
+export const namespaced = true
+
+export const state = {
+    events: [],
+    eventsTotal: 0,
+    event: {}
+
+}
+export const  mutations = {
+    ADD_EVENT(state, event){
+      state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_EVENTS_TOTAL(state, eventsTotal) {
+      state.eventsTotal = eventsTotal
+    },
+    SET_EVENT(state, event) {
+      state.event = event
+    }
+}
+
+export const  actions = {
+    createEvent({commit}, event){
+    // createEvent({commit, state}, event){
+      // how to we access another state in another module
+      // example:
+      // console.log("user state is +" state.user.user.name)
+      // it's not going to work, because the state only local module (state is in event module)
+      // you shoule using rootState instead of using state
+      // createEvent({commit, rootState}, event){
+        // console.log("user state is +" rootState.user.user.name)
+        // rootState is state in store/index.js, so we can access event.js or user.js
+
+        // how do we dispatch user.js action in event.js action
+        // createEvent({commit, dispatch, rootState}, event){
+        //  dispatch('actionWeNeedToCall')
+        // If you want to call an action inside another namespaced module, you’ll need to use the action’s module name, provide a payload as the second argument (null if there is none), and pass { root: true } as the third argument, like so:
+        // dispatch('moduleName/actionToCall', null, { root: true })
+
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    },
+    fetchEvents({commit}, {perPage, page}) {
+      EventService.getEvents(perPage, page)
+      .then((response) => {
+        commit(
+          'SET_EVENTS_TOTAL',
+          parseInt(response.headers['x-total-count'])
+        )
+        console.log(1)
+        commit('SET_EVENTS',response.data);
+      })
+      .catch((error) => {
+        console.log("there was an error " + error.respone);
+      });
+    },
+    fetchEvent({ commit, getters }, id) {
+      var event = getters.getEventById(id) // See if we already have this event
+      if (event) { // If we do, set the event
+        commit('SET_EVENT', event)
+      } else {  // If not, get it with the API.
+        EventService.getEvent(id)
+          .then(response => {
+            commit('SET_EVENT', response.data)
+          })
+          .catch(error => {
+            console.log('There was an error:', error.response)
+          })
+      }
+    }
+}
+export const  getters = {
+    catLength: state => state.categories.length,
+    doneTodos: state => state.todos.filter(todos => todos.done),
+    activeTods: state => state.todos.filter(todos => !todos.done),
+    getEventById: state => id => {
+      console.log(id)
+      return state.events.find(event => event.id === id)
+    },
+}
