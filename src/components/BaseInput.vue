9e4f7fd3c2bceefe610f780f076444ac54a8acd0
Phan Quoc Thanh <thanhpq@thanhphan.local>
2021-03-31 14:04:01 +0700

    refactor code with using baseInput field

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label v-if="label"> {{ label }} </label>
+    <input type="text" :value="value" @input="updateValue" v-bind="$attrs">
+  </div>
+</template>
+
+<script>
+  export default {
+    inheritAttrs: false,
+    props: {
+      label: {
+        type: String,
+        default: ''
+      },
+      value: [String, Number]
+    },
+    methods: {
+      updateValue (event) {
+        this.$emit('input', event.target.value)
+      }
+      // when input typing something, it call updataValue methods, send payload (event.target.value)
+      //  to event name input
+      // v-model have event listening emit like @input. And it set payload to event.title
+      //
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
